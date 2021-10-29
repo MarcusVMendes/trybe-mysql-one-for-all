219e80167ca0d5e2b8c3517f78e7954a6b3146cd
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-10-29 13:23:02 -0300

    Requisito 1 concluido

--- desafio1.sql
+++ desafio1.sql
@@ -4,70 +4,76 @@ CREATE DATABASE `SpotifyClone`;
 
 USE `SpotifyClone`;
 
-CREATE TABLE `users` (
-    user_id INT(11) PRIMARY KEY AUTO_INCREMENT,
-	user_name VARCHAR(50) NOT NULL,
-    age INT(11) NOT NULL,
-    signature_id INT(11) NOT NULL,
-    FOREIGN KEY(signature_id) REFERENCES premium(signature_id)
-) ENGINE=InnoDB;
-
-CREATE TABLE `artists` (
-    artist_id INT(11) PRIMARY KEY AUTO_INCREMENT,
-    artist_name VARCHAR(50) NOT NULL
-) ENGINE=InnoDB;
-
-CREATE TABLE `albums`(
-	album_id INT(11) PRIMARY KEY AUTO_INCREMENT,
-    album_name VARCHAR(50) NOT NULL,
-    artist_id INT(11) NOT NULL,
-    FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
-) ENGINE=InnoDB;
-
-CREATE TABLE `premium` (
-	signature_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE premium (
+	signature_id INT PRIMARY KEY AUTO_INCREMENT,
 	signature VARCHAR(50) NOT NULL,
     signature_price DECIMAL(5,2) NOT NULL
 ) ENGINE=InnoDB;
 
-CREATE TABLE `songs` (
-	song_id INT(11) PRIMARY KEY AUTO_INCREMENT,
-    song_name VARCHAR(50) NOT NULL,
-    album_id INT(11) NOT NULL,
-    FOREIGN KEY(album_id) REFERENCES albums(album_id)
+CREATE TABLE users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+	user_name VARCHAR(50) NOT NULL,
+    age INT NOT NULL,
+    signature_id INT NOT NULL,
+    FOREIGN KEY (signature_id) REFERENCES premium(signature_id)
 ) ENGINE=InnoDB;
 
-CREATE TABLE `reproduction_history` (
-	user_id INT(11) NOT NULL,
-    song_id INT(11) NOT NULL,
+CREATE TABLE artists (
+    artist_id INT PRIMARY KEY AUTO_INCREMENT,
+    artist_name VARCHAR(50) NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE albums (
+	album_id INT PRIMARY KEY AUTO_INCREMENT,
+    album_name VARCHAR(50) NOT NULL,
+    artist_id INT NOT NULL,
+    FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
+) ENGINE=InnoDB;
+
+CREATE TABLE songs (
+	song_id INT PRIMARY KEY AUTO_INCREMENT,
+    song_name VARCHAR(50) NOT NULL,
+    album_id INT NOT NULL,
+    FOREIGN KEY (album_id) REFERENCES albums(album_id)
+) ENGINE=InnoDB;
+
+CREATE TABLE reproduction_history (
+	user_id INT NOT NULL,
+    song_id INT NOT NULL,
     PRIMARY KEY(user_id, song_id),
     FOREIGN KEY(user_id) REFERENCES users(user_id),
     FOREIGN KEY(song_id) REFERENCES songs(song_id)
 ) ENGINE=InnoDB;
 
-CREATE TABLE `follows` (
-	user_id INT(11) NOT NULL,
-    artist_id INT(11) NOT NULL,
+CREATE TABLE follows (
+	user_id INT NOT NULL,
+    artist_id INT NOT NULL,
     PRIMARY KEY(user_id, artist_id),
     FOREIGN KEY(user_id) REFERENCES users(user_id),
     FOREIGN KEY(artist_id) REFERENCES artists(artist_id)
 ) ENGINE=InnoDB;
 
-INSERT INTO SpotifyClone.`users` (user_name, age, signature_id)
+INSERT INTO SpotifyClone.premium (signature, signature_price)
+VALUES
+    ('gratuito', 0),
+    ('universitário', 5.99),
+    ('familiar', 7.99);
+    
+INSERT INTO SpotifyClone.users(user_name, age, signature_id)
 VALUES
     ('Thati', 23, 1),
     ('Cintia', 35, 3),
     ('Bill', 20, 2),
     ('Roger', 45, 1);
 
-INSERT INTO SpotifyClone.`artists`(artist_name)
+INSERT INTO SpotifyClone.artists (artist_name)
 VALUES
     ('Walter Phoenix'),
     ('Peter Strong'),
     ('Lance Day'),
     ('Freedie Shannon');
 
-INSERT INTO SpotifyClone.`albums`(album_name, artist_id)
+INSERT INTO SpotifyClone.albums (album_name, artist_id)
 VALUES
     ('Envious', 1),
     ('Exuberant', 1),
@@ -75,13 +81,7 @@ VALUES
     ('Incandescent', 3),
     ('Temporary Culture', 4);
 
-INSERT INTO SpotifyClone.`premium`(signature, signature_price)
-VALUES
-    ('gratuito', 0),
-    ('universitário', 5,99),
-    ('familiar', 7,99);
-
-INSERT INTO SpotifyClone.`songs`(song_name, album_id)
+INSERT INTO SpotifyClone.songs (song_name, album_id)
 VALUES
     ('Soul For Us', 1),
     ('Reflections Of Magic', 1),
@@ -90,19 +90,19 @@ VALUES
     ('Time Fireworks', 2),
     ('Magic Circus', 3),
     ('Honey, So Do I', 3),
-    ('Sweetie, 'Let's Go Wild' 3),
+    ('Sweetie, Let''s Go Wild', 3),
     ('She Knows', 3),
     ('Fantasy For Me', 4),
     ('Celebration Of More', 4),
     ('Rock His Everything', 4),
     ('Home Forever', 4),
     ('Diamond Power', 4),
-    ('Honey, 'Let's Be Silly', 4),
+    ('Honey, Let''s Be Silly', 4),
     ('Thang Of Thunder', 5),
     ('Words Of Her Life', 5),
     ('Without My Streets', 5);
 
-INSERT INTO SpotifyClone.`reproduction_history`(user_id, song_id)
+INSERT INTO SpotifyClone.reproduction_history (user_id, song_id)
 VALUES
     (1, 1),
     (1, 6),
@@ -119,7 +119,7 @@ VALUES
     (4, 18),
     (4, 11);
 
-INSERT INTO SpotifyClone.`follows`(user_id, artist_id)
+INSERT INTO SpotifyClone.follows (user_id, artist_id)
 VALUES
     (1, 1),
     (1, 4),
@@ -136,4 +136,5 @@ VALUES
 
   -- single quote problem at song 'Sweetie, Let's Go Wild'
   -- https://qastack.com.br/programming/1912095/how-to-insert-a-value-that-contains-an-apostrophe-single-quote
+  -- https://stackoverflow.com/questions/1586560/how-do-i-escape-a-single-quote-in-sql-server
   
